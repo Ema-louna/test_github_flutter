28dc5bde6439054a9d79c453281b8be2d3bd0e83
Ema-louna <emmanuellem26@gmail.com>
2025-10-24 07:22:04 -0400

    Step 7 DONE , Actions cars completed DebugBanner False

--- lib/main.dart
+++ lib/main.dart
@@ -18,11 +18,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page', carsBox: carsBox),
+      home: MyHomePage(title: 'Cars for sale page', carsBox: carsBox),
     );
   }
 }
